0b94e489e0d4d0729079bf05e226d7da41a0faa0
jesus <jesuserner_24@alu.uabcs.mx>
2026-06-08 00:34:42 -0700

    Solucion de error en formateo

--- contracts/DinoCoin.sol
+++ contracts/DinoCoin.sol
@@ -1,43 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-//Importación de contratos openzeppelin 
+//Importación de contratos openzeppelin
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 
-contract DinoCoin is ERC20, ERC20Burnable, ERC20Pausable, Ownable{
-    //Definición de constantes, el suministro masximo se definió en 5,000,000 de tokens, 
+contract DinoCoin is ERC20, ERC20Burnable, ERC20Pausable, Ownable {
+    //Definición de constantes, el suministro masximo se definió en 5,000,000 de tokens,
     //ademas tambien se definio el evento de blacklist para que se puede emitir un cambio de estado al usar la funcion
-    uint public constant MAX_SUPPLY = 5000000 * 10**18;
+    uint256 public constant MAX_SUPPLY = 5000000 * 10 ** 18;
     mapping(address => bool) public blacklisted;
 
     event AddressBlacklisted(address indexed account, bool isBlacklisted);
-    
+
     //Constructor en el que definimos el nombre y simbolo de la criptomoneda
-    constructor()
-        ERC20("DinoCoin","DNC")
-        Ownable(msg.sender)
-    {
+    constructor() ERC20("DinoCoin", "DNC") Ownable(msg.sender) {
         //El deployer obtiene 100,000 tokens
-        _mint(msg.sender, 100000*10**18);
+        _mint(msg.sender, 100000 * 10 ** 18);
     }
-    
+
     //Funcion de minteo en la que solo el owner del contrato puede acuñar más tokens
     //además se define que no se puede superar el suministro maximo
     function mint(address to, uint256 amount) external onlyOwner {
         require(totalSupply() + amount <= MAX_SUPPLY, "Max supply reached");
         _mint(to, amount);
-
     }
 
     //Funcion blacklist y blacklistRemove para bloquear y desbloquear ciertas cuentas y que no puedan interactuar con el contrato
     function blacklist(address _addr) external onlyOwner {
         require(_addr != address(0), "Direccion invalida");
         require(!blacklisted[_addr], "Ya esta en la lista negra");
-        
+
         blacklisted[_addr] = true;
         emit AddressBlacklisted(_addr, true);
     }
@@ -49,16 +45,18 @@ contract DinoCoin is ERC20, ERC20Burnable, ERC20Pausable, Ownable{
     }
 
     //Función pause para que nadie pueda interactuar con el contrato a discreción del owner
-    function pause() external onlyOwner { _pause(); }
-    function unpause() external onlyOwner { _unpause(); }
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
+    }
 
     //Modificadores para evitar transacciones a/desde cuentas en la lista negra
-    function _update(address from, address to, uint256 value)
-        internal override(ERC20, ERC20Pausable)
-    {
+    function _update(address from, address to, uint256 value) internal override(ERC20, ERC20Pausable) {
         require(!blacklisted[from], "La cuenta remitente se encuentra bloqueada");
         require(!blacklisted[to], "La cuenta a la que desea enviar se encuentra bloqueada");
         super._update(from, to, value);
     }
-
 }
--- test/DinoCoin.t.sol
+++ test/DinoCoin.t.sol
@@ -15,25 +15,25 @@ contract DinoCoinTest is Test {
         token = new DinoCoin();
     }
 
-    // 1. constructor 
+    // 1. constructor
     function test_Initialization() public view {
         assertEq(token.name(), "DinoCoin");
         assertEq(token.symbol(), "DNC");
         assertEq(token.owner(), owner);
-        assertEq(token.balanceOf(owner), 100000 * 10**18);
+        assertEq(token.balanceOf(owner), 100000 * 10 ** 18);
     }
 
     // 2. test de función mint
     function test_MintAsOwner() public {
         vm.prank(owner);
-        token.mint(user1, 1000 * 10**18);
-        assertEq(token.balanceOf(user1), 1000 * 10**18);
+        token.mint(user1, 1000 * 10 ** 18);
+        assertEq(token.balanceOf(user1), 1000 * 10 ** 18);
     }
 
     function test_MintFailNotOwner() public {
         vm.prank(user1);
         vm.expectRevert(abi.encodeWithSignature("OwnableUnauthorizedAccount(address)", user1));
-        token.mint(user1, 1000 * 10**18);
+        token.mint(user1, 1000 * 10 ** 18);
     }
 
     function test_MintFailMaxSupply() public {
@@ -49,7 +49,7 @@ contract DinoCoinTest is Test {
         vm.expectEmit(true, false, false, true);
         emit DinoCoin.AddressBlacklisted(user1, true);
         token.blacklist(user1);
-        
+
         assertTrue(token.blacklisted(user1));
     }
 
@@ -70,7 +70,7 @@ contract DinoCoinTest is Test {
     function test_RemoveFromBlacklist() public {
         vm.startPrank(owner);
         token.blacklist(user1);
-        
+
         vm.expectEmit(true, false, false, true);
         emit DinoCoin.AddressBlacklisted(user1, false);
         token.blacklistRemove(user1);
@@ -87,14 +87,14 @@ contract DinoCoinTest is Test {
 
     function test_TransferFailWhenFromIsBlacklisted() public {
         vm.prank(owner);
-        token.mint(user1, 1000 * 10**18);
+        token.mint(user1, 1000 * 10 ** 18);
 
         vm.prank(owner);
         token.blacklist(user1);
 
         vm.prank(user1);
         vm.expectRevert("La cuenta remitente se encuentra bloqueada");
-        token.transfer(user2, 500 * 10**18);
+        token.transfer(user2, 500 * 10 ** 18);
     }
 
     function test_TransferFailWhenToIsBlacklisted() public {
@@ -103,22 +103,22 @@ contract DinoCoinTest is Test {
 
         vm.prank(owner);
         vm.expectRevert("La cuenta a la que desea enviar se encuentra bloqueada");
-        token.transfer(user2, 500 * 10**18);
+        token.transfer(user2, 500 * 10 ** 18);
     }
 
     // 4. test de función pause
     function test_PauseAndUnpause() public {
         vm.startPrank(owner);
         token.pause();
-        
+
         // Comprobacion de fallo estando pausado
         vm.expectRevert(abi.encodeWithSignature("EnforcedPause()"));
-        token.transfer(user1, 500 * 10**18);
+        token.transfer(user1, 500 * 10 ** 18);
 
         token.unpause();
-        token.transfer(user1, 500 * 10**18);
+        token.transfer(user1, 500 * 10 ** 18);
         vm.stopPrank();
-        
-        assertEq(token.balanceOf(user1), 500 * 10**18);
+
+        assertEq(token.balanceOf(user1), 500 * 10 ** 18);
     }
 }
